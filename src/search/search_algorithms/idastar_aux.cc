25d5b68b54d6cfc0f75fbcb98c40e3b47e0ecec7
Petr Sabovcik <lalancz77@gmail.com>
2024-04-23 18:11:41 +0200

    idastar algorithm progress

--- src/search/search_algorithms/idastar_aux.cc
+++ src/search/search_algorithms/idastar_aux.cc
@@ -108,177 +108,47 @@ void IDAstar_aux::print_statistics() const {
 }
 
 SearchStatus IDAstar_aux::step() {
-    optional<SearchNode> node;
-    while (true) {
-        if (open_list->empty()) {
-            log << "Completely explored state space -- no solution!" << endl;
-            return FAILED;
-        }
-        StateID id = open_list->remove_min();
-        State s = state_registry.lookup_state(id);
-        node.emplace(search_space.get_node(s));
-
-        if (node->is_closed())
-            continue;
-
-        /*
-          We can pass calculate_preferred=false here since preferred
-          operators are computed when the state is expanded.
-        */
-        EvaluationContext eval_context(s, node->get_g(), false, &statistics);
-
-        if (lazy_evaluator) {
-            /*
-              With lazy evaluators (and only with these) we can have dead nodes
-              in the open list.
-
-              For example, consider a state s that is reached twice before it is expanded.
-              The first time we insert it into the open list, we compute a finite
-              heuristic value. The second time we insert it, the cached value is reused.
-
-              During first expansion, the heuristic value is recomputed and might become
-              infinite, for example because the reevaluation uses a stronger heuristic or
-              because the heuristic is path-dependent and we have accumulated more
-              information in the meantime. Then upon second expansion we have a dead-end
-              node which we must ignore.
-            */
-            if (node->is_dead_end())
-                continue;
-
-            if (lazy_evaluator->is_estimate_cached(s)) {
-                int old_h = lazy_evaluator->get_cached_estimate(s);
-                int new_h = eval_context.get_evaluator_value_or_infinity(lazy_evaluator.get());
-                if (open_list->is_dead_end(eval_context)) {
-                    node->mark_as_dead_end();
-                    statistics.inc_dead_ends();
-                    continue;
-                }
-                if (new_h != old_h) {
-                    open_list->insert(eval_context, id);
-                    continue;
-                }
-            }
-        }
-
-        node->close();
-        assert(!node->is_dead_end());
-        update_f_value_statistics(eval_context);
-        statistics.inc_expanded();
-        break;
-    }
-
     const State &s = node->get_state();
     if (check_goal_and_set_plan(s))
         return SOLVED;
 
-    vector<OperatorID> applicable_ops;
-    successor_generator.generate_applicable_ops(s, applicable_ops);
+    EvaluationContext eval_context(s, node->get_g(), false, &statistics);
+    int h = eval_context.get_evaluator_value_or_infinity(f_evaluator.get());
+    int f = node->get_g() + h;
 
-    /*
-      TODO: When preferred operators are in use, a preferred operator will be
-      considered by the preferred operator queues even when it is pruned.
-    */
-    pruning_method->prune_operators(s, applicable_ops);
-
-    // This evaluates the expanded state (again) to get preferred ops
-    EvaluationContext eval_context(s, node->get_g(), false, &statistics, true);
-    ordered_set::OrderedSet<OperatorID> preferred_operators;
-    for (const shared_ptr<Evaluator> &preferred_operator_evaluator : preferred_operator_evaluators) {
-        collect_preferred_operators(eval_context,
-                                    preferred_operator_evaluator.get(),
-                                    preferred_operators);
-    }
+    if (f > bound)
+        return f;
 
     for (OperatorID op_id : applicable_ops) {
         OperatorProxy op = task_proxy.get_operators()[op_id];
-        if ((node->get_real_g() + op.get_cost()) >= bound)
-            continue;
 
         State succ_state = state_registry.get_successor_state(s, op);
-        statistics.inc_generated();
-        bool is_preferred = preferred_operators.contains(op_id);
+        staistics.inc_generated();
 
         SearchNode succ_node = search_space.get_node(succ_state);
 
         for (Evaluator *evaluator : path_dependent_evaluators) {
-            evaluator->notify_state_transition(s, op_id, succ_state);
+            evaluator->notify_state_transition(s, op, succ_state);
         }
 
-        // Previously encountered dead end. Don't re-evaluate.
-        if (succ_node.is_dead_end())
-            continue;
-
         if (succ_node.is_new()) {
-            // We have not seen this state before.
-            // Evaluate and create a new node.
-
-            // Careful: succ_node.get_g() is not available here yet,
-            // hence the stupid computation of succ_g.
-            // TODO: Make this less fragile.
             int succ_g = node->get_g() + get_adjusted_cost(op);
 
-            EvaluationContext succ_eval_context(
-                succ_state, succ_g, is_preferred, &statistics);
+            EvaluationContext succ_eval_context(succ_state, succ_g, false, &statistics);
             statistics.inc_evaluated_states();
 
-            if (open_list->is_dead_end(succ_eval_context)) {
-                succ_node.mark_as_dead_end();
-                statistics.inc_dead_ends();
-                continue;
-            }
             succ_node.open(*node, op, get_adjusted_cost(op));
 
             open_list->insert(succ_eval_context, succ_state.get_id());
+
+            t = idastar_aux::step();
+
             if (search_progress.check_progress(succ_eval_context)) {
                 statistics.print_checkpoint_line(succ_node.get_g());
                 reward_progress();
             }
-        } else if (succ_node.get_g() > node->get_g() + get_adjusted_cost(op)) {
-            // We found a new cheapest path to an open or closed state.
-            if (reopen_closed_nodes) {
-                if (succ_node.is_closed()) {
-                    /*
-                      TODO: It would be nice if we had a way to test
-                      that reopening is expected behaviour, i.e., exit
-                      with an error when this is something where
-                      reopening should not occur (e.g. A* with a
-                      consistent heuristic).
-                    */
-                    statistics.inc_reopened();
-                }
-                succ_node.reopen(*node, op, get_adjusted_cost(op));
-
-                EvaluationContext succ_eval_context(
-                    succ_state, succ_node.get_g(), is_preferred, &statistics);
-
-                /*
-                  Note: our old code used to retrieve the h value from
-                  the search node here. Our new code recomputes it as
-                  necessary, thus avoiding the incredible ugliness of
-                  the old "set_evaluator_value" approach, which also
-                  did not generalize properly to settings with more
-                  than one evaluator.
-
-                  Reopening should not happen all that frequently, so
-                  the performance impact of this is hopefully not that
-                  large. In the medium term, we want the evaluators to
-                  remember evaluator values for states themselves if
-                  desired by the user, so that such recomputations
-                  will just involve a look-up by the Evaluator object
-                  rather than a recomputation of the evaluator value
-                  from scratch.
-                */
-                open_list->insert(succ_eval_context, succ_state.get_id());
-            } else {
-                // If we do not reopen closed nodes, we just update the parent pointers.
-                // Note that this could cause an incompatibility between
-                // the g-value and the actual path that is traced back.
-                succ_node.update_parent(*node, op, get_adjusted_cost(op));
-            }
         }
     }
-
-    return IN_PROGRESS;
 }
 
 void IDAstar_aux::reward_progress() {
